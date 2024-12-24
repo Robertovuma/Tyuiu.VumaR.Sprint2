08d426f9fecc3736b8fc15e286029fa2bf300f7a
MundoG84 <raimundomacuacua271121@gmail.com>
2024-12-24 05:14:06 +0500

    Task7

---
+++ Tyuiu.VumaR.Sprint2.Task7.V2.Test/Test1.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.VumaR.Sprint2.Task7.V2.Lib;
+namespace Tyuiu.VumaR.Sprint2.Task7.V2.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x = 0.5;
+            double y = -0.3;
+            bool res = ds.CheckDotInShadedArea(x, y);
+            bool wait = true;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.VumaR.Sprint2.Task7.V2/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using Tyuiu.VumaR.Sprint2.Task7.V2.Lib;
+namespace Tyuiu.VumaR.Sprint2.Task7.V2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+
+            double x, y;
+
+            Console.WriteLine("Введите значение переменной x: ");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение переменной y: ");
+            y = Convert.ToDouble(Console.ReadLine());
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+
+            Console.WriteLine("###########################################################################");
+            Console.WriteLine("# РЕЗУЛЬТАТ:                                                              #");
+            Console.WriteLine("###########################################################################");
+
+            if (res) Console.WriteLine("Точка находится в заштрихованной области");
+            else Console.WriteLine("Точка не находится в заштрихованной области");
+
+            Console.ReadKey();
+        }
+    }
+}
